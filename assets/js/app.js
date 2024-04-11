c71ee49796962253bcf09f0d020be0ed6bc329c9
faria-s <salome.faria8@gmail.com>
2024-04-11 15:36:52 +0100

    feat: add syntax highlight

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@
 //
 //     import "some-package"
 //
-
+import hljs from "highlight.js"
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
@@ -25,6 +25,37 @@ import topbar from "../vendor/topbar"
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 let Hooks = {};
+
+Hooks.Highlight = {
+  mounted() {
+    let name = this.el.getAttribute("data-name");
+    let codeBlock = this.el.querySelector("pre code");
+
+    if (name && codeBlock) {
+      codeBlock.className = codeBlock.className.replace(/language-\S+/g, "");
+      codeBlock.classList.add(`language-${this.getSyntaxType(name)}`);
+      hljs.highlightElement(codeBlock);
+    }
+  },
+
+  getSyntaxType(name){
+    let exetension = name.split(".").pop();
+    switch (exetension) {
+      case "txt":
+        return "text"
+      case "json":
+          return "json";
+      case "html" :
+        return "html";
+      case "heex":
+        return "html";
+      case "js":
+        return "javascript";
+      default:
+        return "elixir";
+    }
+  }
+};
 
 Hooks.UpdateLineNumbers = {
   mounted(){
